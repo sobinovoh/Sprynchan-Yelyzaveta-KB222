d5aa4e6fd26041ecc154b6cd5e4273932e8642e5
SprynchanYelysaveta <lizok.sprynchan@gmail.com>
2023-02-28 20:35:36 +0200

    Added Sprynchan_task

---
+++ lab08/prj/Sprynchan_task/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "ModulesSprynchan.h"
+#include <Windows.h>
+
+using namespace std;
+
+dev() {
+    cout << "\u00A9" << "Sprynchan Yelysaveta" << endl;
+    return 0;
+}
+
+logical_exp (char a, char b) {
+    if (a + 1 == b + 2)
+    {
+        cout << "false" << endl;
+    }
+    else
+    {
+        cout << "true" << endl;
+    }
+    return 0;
+}
+
+dec_and_hec(int x, int y, int z) {
+    cout << "В десятковій с. ч.: " << x << ", " << y << ", " << z << endl;
+    printf("В шістнадцятковій с. ч.: %X, %X, %X\n", x, y, z);
+    return 0;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    dev();
+    char a, b;
+    cout << "Введіть значення a, b: " << endl;
+    cin >> a >> b;
+    logical_exp (a, b);
+    float x, y, z;
+    cout << "Введіть значення x, y, z: " << endl;
+    cin >> x >> y >> z;
+    dec_and_hec(x, y, z);
+    cout << "Обчислене значення S: " << s_calculation(x, y, z);
+    return 0;
+}
